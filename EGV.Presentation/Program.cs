69a030e1d54c9f2287b518ce2741a621b77e21e7
Van Nguyen Ngoc Gia <van.nguyenngocgia@nashtechglobal.com>
2022-11-23 11:47:39 +0700

    update program presentation

--- EGV.Presentation/Program.cs
+++ EGV.Presentation/Program.cs
@@ -1,10 +1,15 @@
+using System.Net;
 using EGV.Business;
 using EGV.DataAccessor;
+using EGV.DataAccessor.Entities;
 using EGV.Presentation.ServiceCollection;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.IdentityModel.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddAuthenticationAndAuthorizationRegister();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddDataAccessorLayer(builder.Configuration);
@@ -13,7 +18,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddEndpointsApiExplorer();
+
+// builder.Services.AddSpaStaticFiles(configuration =>
+// {
+//     configuration.RootPath = "adminweb/build";
+// });
+
 builder.Services.AddSwaggerRegister();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigins",
+        builder =>
+        {
+            //https://asset-management-team3-group1.azurewebsites.net/
+            builder.WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -25,13 +48,30 @@ if (app.Environment.IsDevelopment())
     {
         options.OAuthUsePkce();
     });
+    IdentityModelEventSource.ShowPII = true;
+    ServicePointManager.Expect100Continue = true;
+    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+    | SecurityProtocolType.Tls11
+    | SecurityProtocolType.Tls12;
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers().RequireAuthorization("ApiScope");
+
+// app.UseSpa(spa =>
+// {
+//     spa.Options.SourcePath = "adminweb";
+
+//     if (app.Environment.IsDevelopment())
+//     {
+//         spa.UseReactDevelopmentServer(npmScript: "start");
+//     }
+// });
 
 app.Run();
